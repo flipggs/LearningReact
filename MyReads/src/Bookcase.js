373c495ba4640c816dd811d8fbb17e1aab6bf08c
flipggs <flipggs@gmail.com>
2017-10-21 13:48:40 -0200

    Separação dos componentes e criação das rotas

---
+++ MyReads/src/Bookcase.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import Book from './Book'
+
+class Bookcase extends Component {
+
+    render() {
+        return (
+            <div className="list-books">
+                <div className="list-books-title">
+                    <h1>MyReads</h1>
+                </div>
+                <div className="list-books-content">
+                    <div>
+                        <div className="bookshelf">
+                            <h2 className="bookshelf-title">Currently Reading</h2>
+                            <div className="bookshelf-books">
+                                <ol className="books-grid">
+                                    <li>
+                                        <Book />
+                                    </li>
+                                </ol>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div className="open-search">
+                    <Link to="/search" >Add a book</Link>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Bookcase
