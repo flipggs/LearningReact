7e07b589b7769e5ebcf0c594a3687f4e5bafb078
flipggs <flipggs@gmail.com>
2017-11-15 23:18:04 -0200

    Update app

--- Udacimeals/src/components/App.js
+++ Udacimeals/src/components/App.js
@@ -1,49 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
-import { addRecipe } from './../actions/index';
 
 class App extends Component {
-
-  state = {
-    calendar: null
-  }
-
-  componentDidMount() {
-    const { store } = this.props
-
-    store.subscribe(() => {
-      this.setState(() => ({
-        calendar: store.getState()
-      }))
-    })
-  }
-
-  submitFood = () => {
-    this.props.store.dispatch(addRecipe({
-      day: 'monday',
-      meal: 'breakfast',
-      recipe: {
-        label: this.input.value
-      }
-    }))
-
-    this.input.value = ''
-  }
-
   render() {
     return (
       <div>
-        <input
-          type="text"
-          ref={(input) => this.input = input}
-          placeholder="Monday's Breackfast"
-        />
-
-        <button onClick={this.submitFood}>Submit</button>
-
-        <pre>
-          Monday's Breakfast: {this.state.calendar && this.state.calendar.monday.breakfast}
-        </pre>
+        Hello World        
       </div>
     );
   }
